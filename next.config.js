7ae42ae939bc69d58b9febd9bcf974165b1ce840
Taha Polat <taha.polat@tgs.aero>
2024-05-06 15:57:31 +0300

    next.config configurated for file uploading in DB.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ciupload.tgs.aero',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig;
